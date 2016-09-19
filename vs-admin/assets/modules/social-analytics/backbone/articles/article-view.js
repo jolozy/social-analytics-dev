6a19a0340f30c14061313e4f299850c9bce8655e
jolozy <joannelozy@outlook.com>
2016-09-19 22:46:43 +0800

    latest commits

--- vs-admin/assets/modules/social-analytics/backbone/articles/article-view.js
+++ vs-admin/assets/modules/social-analytics/backbone/articles/article-view.js
@@ -29,91 +29,92 @@ define(["backbone", "moment", "underscore", "jquery", "tpl!./article-template.tp
           },
 
           /////TRIES AARON'S RECOMMENDATION/////
-          // bindEvents: function(e){
-          //   var _this = this
-          //   $('.paginated-page').on("click",function(e){
-          //     e.preventDefault();
-          //     _this.setSelectedPaginatedPage( $(this) ); //gives selected page button an active class
-          //     _this.fetchAll( $(this) ); //fetch all from Mock and Buzz
-          //   });
-          //   // //tried the below method too, but didn't work:
-          //   //$('.paginated-page').on("click", this.setSelectedPaginatedPage);
-          //   //$('.paginated-page').on("click",_this.fetchAll);
-          // },
+          bindEvents: function(e){
+            var _this = this
 
-          // setSelectedPaginatedPage: function($el){
-          //   console.log($el);
-          //   $('.paginated-page.selected').removeClass('selected');
-          //   $el.addClass('selected'); //DO: have to write a CSS class that makes a page button active!!
-          // },
+            $(document).on('click', 'paginated-page', function(e){
+              e.preventDefault();
+              _this.setSelectedPaginatedPage( $(this) ); //gives selected page button an active class
+              _this.fetchAll( $(this) ); //fetch all from Mock and Buzz
+            });
+            // //tried the below method too, but didn't work:
+            //$('.paginated-page').on("click", this.setSelectedPaginatedPage);
+            //$('.paginated-page').on("click",_this.fetchAll);
+          },
 
-          // fetchAll: function($el){
-          //   var _this = this;
-          //   var PER_PAGE = 10;
-          //   var OFFSET = $el.attr('data-value');
-          //   _this.myBuzzInfo.url = 'http://buzz.viddsee.com/api/buzz/v1/posts?offset' + OFFSET + '&per_page' + PER_PAGE; //change buzz collection's URL
+          setSelectedPaginatedPage: function($el){
+            console.log($el);
+            $('.paginated-page.selected').removeClass('selected');
+            ($el).addClass('selected'); //DO: have to write a CSS class that makes a page button active!!
+          },
 
-            ////Check AJAX calls
-            //$.when( _this.myBuzzInfo.fetch(), _this.myArticles.fetch() ).done(function(buzzData, articleData){
-            //  _this.render();
-            // });
+          fetchAll: function($el){
+            var _this = this;
+            var PER_PAGE = 10;
+            var OFFSET = ($el).attr('data-value');
+            _this.myBuzzInfo.url = 'http://buzz.viddsee.com/api/buzz/v1/posts?offset' + OFFSET + '&per_page' + PER_PAGE; //change buzz collection's URL
+
+            //Check AJAX calls
+            $.when( _this.myBuzzInfo.fetch(), _this.myArticles.fetch() ).done(function(buzzData, articleData){
+             _this.render();
+            });
 
 
-          fetchAll: function(){
-            var that = this;
-
-            $.when( fetchMyBuzzInfo(), fetchMyArticles() ).done(function(buzzData, articleData){
-              //posts from Buzz Viddsee
-              that.myBuzzInfo = buzzData[0].posts
-              _.each((buzzData[0].posts), function(post, index){
-                (buzzData[0].posts)[index].uid = 'buzz_article_' + post.ID;
-              });
-              //posts from Mock API
-              that.myArticles = articleData[0]
-
-              that.render();
-              }); //ends both checks for AJAX calls
-
-            function fetchMyBuzzInfo(){ //AJAX call for Buzz Viddsee
-                var PER_PAGE = 5;
-                var OFFSET = ( $(event.target).data('value') -1) *PER_PAGE +1;
-                var paginatedURL = 'http://buzz.viddsee.com/api/buzz/v1/posts?offset=' + OFFSET + '&per_page=' + PER_PAGE; //change buzz collection's URL
-                console.log(paginatedURL);
-                return $.ajax({
-                  url: paginatedURL,
-                  data: {
-                    format: 'json'
-                  },
-                  //current_page: event.target.innerHTML,
-                  //total: 2180,
-                  error: function(){
-                    console.log("error!");
-                  },
-                  //dataType: 'jsonp',
-                  success: function(data){
-                    console.log('successful!');
-                  },
-                  type: 'GET'
-                });
-              }
-            function fetchMyArticles(){ //AJAX call for Mock API
-              return $.ajax({
-                url: 'https://demo4828724.mockable.io/articles',
-                data: {
-                  format: 'json'
-                },
-                error: function(){
-                  console.log("error!");
-                  $
-                },
-                //dataType: 'jsonp',
-                success: function(data){
-                  console.log('successful!');
-                },
-                type: 'GET'
-              });
-            }
-
+          // fetchAll: function(){
+          //   var that = this;
+          //
+          //   $.when( fetchMyBuzzInfo(), fetchMyArticles() ).done(function(buzzData, articleData){
+          //     //posts from Buzz Viddsee
+          //     that.myBuzzInfo = buzzData[0].posts
+          //     _.each((buzzData[0].posts), function(post, index){
+          //       (buzzData[0].posts)[index].uid = 'buzz_article_' + post.ID;
+          //     });
+          //     //posts from Mock API
+          //     that.myArticles = articleData[0]
+          //
+          //     that.render();
+          //     }); //ends both checks for AJAX calls
+          //
+          //   function fetchMyBuzzInfo(){ //AJAX call for Buzz Viddsee
+          //       var PER_PAGE = 5;
+          //       var OFFSET = ( $(event.target).data('value') -1) *PER_PAGE +1;
+          //       var paginatedURL = 'http://buzz.viddsee.com/api/buzz/v1/posts?offset=' + OFFSET + '&per_page=' + PER_PAGE; //change buzz collection's URL
+          //       console.log(paginatedURL);
+          //       return $.ajax({
+          //         url: paginatedURL,
+          //         data: {
+          //           format: 'json'
+          //         },
+          //         //current_page: event.target.innerHTML,
+          //         //total: 2180,
+          //         error: function(){
+          //           console.log("error!");
+          //         },
+          //         //dataType: 'jsonp',
+          //         success: function(data){
+          //           console.log('successful!');
+          //         },
+          //         type: 'GET'
+          //       });
+          //     }
+          //   function fetchMyArticles(){ //AJAX call for Mock API
+          //     return $.ajax({
+          //       url: 'https://demo4828724.mockable.io/articles',
+          //       data: {
+          //         format: 'json'
+          //       },
+          //       error: function(){
+          //         console.log("error!");
+          //         $
+          //       },
+          //       //dataType: 'jsonp',
+          //       success: function(data){
+          //         console.log('successful!');
+          //       },
+          //       type: 'GET'
+          //     });
+          //   }
+          //
           } //end fetchAll
 
       }); //end ArticleView
